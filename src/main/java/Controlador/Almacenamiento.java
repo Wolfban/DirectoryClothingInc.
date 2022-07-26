e66c7b9247ac7d51dfe7fc44f6bf73518b4613c6
Wolfban <estebanbarrera104@gmail.com>
2022-07-26 14:50:46 -0600

    Se crean los tres ArrayList de los tres distintos Modelos.

--- src/main/java/Controlador/Almacenamiento.java
+++ src/main/java/Controlador/Almacenamiento.java
@@ -1,4 +1,14 @@
 package Controlador;
+import Modelo.Camisas;
+import Modelo.Usuarios;
+import Modelo.Catalogos;
+
+import java.util.ArrayList;
 
 public class Almacenamiento {
+    public static ArrayList<Usuarios> poolusuarios = new ArrayList<>();
+    public static  ArrayList<Camisas> poolcamisas = new ArrayList<>();
+
+
+    public  static  ArrayList<Catalogos> poolcatalogos = new ArrayList<>();
 }

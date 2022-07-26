215ccabbc5014d303857ffc6c0a471a89e00f4ae
Wolfban <estebanbarrera104@gmail.com>
2022-07-26 14:32:13 -0600

    Se enlaza AgregarClientes con su respectivo FXML

--- src/main/java/com/example/directoryclothinginc/AgregarClientes.java
+++ src/main/java/com/example/directoryclothinginc/AgregarClientes.java
@@ -1,13 +1,71 @@
 package com.example.directoryclothinginc;
 
+import Modelo.Usuarios;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class AgregarClientes  {
+
+    @FXML
+    private TableColumn ColmApellido1;
+
+    @FXML
+    private TableColumn ColmApellido2;
+
+    @FXML
+    private TableColumn ColmDireccion;
+
+    @FXML
+    private TableColumn ColmEmail;
+
+    @FXML
+    private TableColumn ColmNombre;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private TableColumn colmID;
+
+    @FXML
+    private TableView<Usuarios> tblUsusarios;
+
+    @FXML
+    private TextField txtApellido1;
+
+    @FXML
+    private TextField txtApellido2;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtNombre;
+
+    ObservableList<Usuarios> usuarios = FXCollections.observableArrayList();
 
 
 

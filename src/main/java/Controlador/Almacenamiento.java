5fad1fac68abe445d01ea12c520a0bcfbc4f00c6
Wolfban <estebanbarrera104@gmail.com>
2022-07-26 14:52:35 -0600

    Se crea la funcion de agregarCliente y el RevisarID para poder agregar el Cliente al pool.

--- src/main/java/Controlador/Almacenamiento.java
+++ src/main/java/Controlador/Almacenamiento.java
@@ -8,7 +8,29 @@ import java.util.ArrayList;
 public class Almacenamiento {
     public static ArrayList<Usuarios> poolusuarios = new ArrayList<>();
     public static  ArrayList<Camisas> poolcamisas = new ArrayList<>();
-
-
     public  static  ArrayList<Catalogos> poolcatalogos = new ArrayList<>();
+
+    public static boolean RevisarID(int id){
+        boolean DobleID = false;
+        for(int i = 0; i< poolusuarios.size(); i++){
+            if(id == poolusuarios.get(i).getID()){
+                DobleID = true;
+            }
+        }
+        return DobleID;
+    }
+
+    public static boolean agregarCliente (String nombre, String primerApellido, String segundoApellido, String direccion, String correo, int id) {
+        boolean hayError = false;
+        Usuarios usuarionew = new Usuarios(nombre, primerApellido, segundoApellido, direccion, correo, id);
+        if (poolusuarios.size() == 0){
+            poolusuarios.add(usuarionew);
+
+        } else if (!RevisarID(id)) {
+            poolusuarios.add(usuarionew);
+        }else{
+            hayError = true;
+        }
+        return hayError;
+    }
 }

6f088383b818a45ffe95fa6164d96d8eb19d4bb6
Wolfban <estebanbarrera104@gmail.com>
2022-07-26 14:23:51 -0600

    Se enlaza el Menu controlador con el Menu FXML

--- src/main/java/com/example/directoryclothinginc/Menu.java
+++ src/main/java/com/example/directoryclothinginc/Menu.java
@@ -1,4 +1,34 @@
 package com.example.directoryclothinginc;
 
-public class Menu {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+public class Menu implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private Button btnCAMISETA;
+
+    @FXML
+    private Button btnCatalogo;
+
+    @FXML
+    private Button btnClientes;
+
 }
